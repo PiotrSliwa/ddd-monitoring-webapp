5268284ddfb982314c595ffaf70e9b7c9c545f8d
PiotrSliwa <iodosite@gmail.com>
2019-10-08 17:59:28 +0200

    minor refactor

--- src/main/java/tech/eversoft/chartsapp/presenter/domain/DayByDayPresentationRepository.java
+++ src/main/java/tech/eversoft/chartsapp/presenter/domain/DayByDayPresentationRepository.java
@@ -3,6 +3,7 @@ package tech.eversoft.chartsapp.presenter.domain;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import tech.eversoft.chartsapp.availability.domain.Availability;
 import tech.eversoft.chartsapp.availability.domain.AvailabilityIndex;
 import tech.eversoft.chartsapp.availability.domain.AvailabilityRepository;
 
@@ -19,18 +20,23 @@ public class DayByDayPresentationRepository {
     public DayByDayPresentation findByCompanyNameEqualAndDateBetween(String companyName, LocalDateTime beginning, LocalDateTime end) {
         var beginningMidnight = beginning.truncatedTo(ChronoUnit.DAYS);
         var availabilities = availabilityRepository.findByCompanyNameEqualAndDataDateBetween(companyName, beginningMidnight, end);
-        var indexesGroupedByDay = new HashMap<LocalDateTime, List<AvailabilityIndex>>();
-        for (var availability : availabilities) {
-            var dayGroup = indexesGroupedByDay.computeIfAbsent(availability.getDate().getDateTime().truncatedTo(ChronoUnit.DAYS),
-                                                               d -> new ArrayList<>());
-            dayGroup.add(availability.getCurrentIndex());
-        }
+        var indexesGroupedByDay = groupByDay(availabilities);
         var dayByDayAvailability = new ArrayList<AvailabilityIndex>();
         for (var day = beginningMidnight; day.isBefore(end); day = day.plusDays(1)) {
             var dayGroup = indexesGroupedByDay.get(day);
             dayByDayAvailability.add(getDayAvailability(dayGroup));
         }
         return new DayByDayPresentation(dayByDayAvailability);
+    }
+
+    private static Map<LocalDateTime, List<AvailabilityIndex>> groupByDay(List<Availability> availabilities) {
+        var indexesGroupedByDay = new HashMap<LocalDateTime, List<AvailabilityIndex>>();
+        for (var availability : availabilities) {
+            var dayGroup = indexesGroupedByDay.computeIfAbsent(availability.getDate().getDateTime().truncatedTo(ChronoUnit.DAYS),
+                                                               d -> new ArrayList<>());
+            dayGroup.add(availability.getCurrentIndex());
+        }
+        return indexesGroupedByDay;
     }
 
     private static AvailabilityIndex getDayAvailability(List<AvailabilityIndex> dayGroup) {
